10dfbdd5f1676c50274c1bd51d3891a8d11a46b0
Hgcw888 <17127446565@qq.com>
2021-05-04 15:20:27 +0800

    打包

--- src/test/java/com/hgcw/wiki/WikiApplicationTests.java
+++ src/test/java/com/hgcw/wiki/WikiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.hgcw.wiki;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WikiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.hgcw.wiki;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WikiApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
